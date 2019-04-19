0586c60ab613ce036ee5befb72219cc25f9c31bf
Ramson <ramson.yasin@gmail.com>
2019-04-18 22:48:05 -0400

    Setting up the App layout

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -2,8 +2,18 @@ package com.example.braintrainer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button goBtn;
+
+
+    public void  start(View view){
+        goBtn = findViewById(R.id.goButton);
+        goBtn.setVisibility(View.INVISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
